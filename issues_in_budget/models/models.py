cb555d67e7a34f931bbbe94a22046ed6d5dd3ecc
William <williamcolin954@gmail.com>
2019-05-16 16:10:59 -0500

    se quito el .line

--- issues_in_budget/models/models.py
+++ issues_in_budget/models/models.py
@@ -5,7 +5,7 @@ from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationE
 from datetime import datetime, date, time
 
 class TablaPresupuesto(models.Model):
-    _inherit = "crossovered.budget.line"
+    _inherit = "crossovered.budget"
 
     variacion_presupuesto = fields.Integer(string="Variación vs Presupuesto",compute="variation")
 
